27ed7e4f19eede1d641d8b3325754a780c51851b
Sergey Moshnikov <s.moshnikov@gmail.com>
2025-06-12 10:46:47 +0200

    Decoding the entire dict

--- src/decode_dict.rs
+++ src/decode_dict.rs
@@ -12,34 +12,71 @@ impl<'a> Dict<'a> {
     }
 }
 
+struct DecoderState<'a> {
+    data: &'a [u8],
+    total_consumed_length: usize,
+}
+
+impl<'a> DecoderState<'a> {
+    pub fn new(rest_data: &'a [u8]) -> Self {
+        Self {
+            data: rest_data,
+            total_consumed_length: 0,
+        }
+    }
+
+    fn move_by(&mut self, offset: usize) {
+        self.data = &self.data[offset..];
+        self.total_consumed_length += offset;
+    }
+}
+
 pub fn decode_dict(encoded: &[u8]) -> Result<(Dict, usize), Box<dyn Error>> {
     let mut values = HashMap::new();
-    let mut rest_data = &encoded[1..];
-    let mut total_consumed_length = 1;
+    let mut state = DecoderState::new(&encoded[1..]);
 
-    while rest_data[0] != b'e' {
-        let (key, consumed_length) = decode_string(rest_data)?;
-        rest_data = &rest_data[consumed_length..];
-        total_consumed_length += consumed_length;
+    while state.data[0] != b'e' {
+        let (key, consumed_length) = decode_string(state.data)?;
+        state.move_by(consumed_length);
 
-        if rest_data[0] == b'i' {
-            let end_index = rest_data.iter().position(|&b| b == b'e').unwrap();
-            rest_data = &rest_data[end_index + 1..];
-            total_consumed_length += end_index + 1;
-        } else if rest_data[0] == b'd' {
-            let (dict, consumed_length) = decode_dict(rest_data)?;
-            rest_data = &rest_data[consumed_length..];
-            total_consumed_length += consumed_length;
+        if state.data[0] == b'i' {
+            let end_index = state.data.iter().position(|&b| b == b'e').unwrap();
+            state.move_by(end_index + 1);
+        } else if state.data[0] == b'd' {
+            let (dict, consumed_length) = decode_dict(state.data)?;
+            state.move_by(consumed_length);
+        } else if state.data[0] == b'l' {
+            decode_list(&mut state)?;
         } else {
-            let (value, consumed_length) = decode_string(rest_data)?;
-            rest_data = &rest_data[consumed_length..];
-            total_consumed_length += consumed_length;
+            let (value, consumed_length) = decode_string(state.data)?;
+            state.move_by(consumed_length);
 
             values.insert(key, value);
         }
     }
 
-    return Ok((Dict { values }, total_consumed_length + 1));
+    return Ok((Dict { values }, state.total_consumed_length + 2));
+}
+
+fn decode_list(state: &mut DecoderState) -> Result<(), Box<dyn Error>> {
+    state.move_by(1);
+    while state.data[0] != b'e' {
+        if state.data[0] == b'i' {
+            let end_index = state.data.iter().position(|&b| b == b'e').unwrap();
+            state.move_by(end_index + 1);
+        } else if state.data[0] == b'd' {
+            let (dict, consumed_length) = decode_dict(state.data)?;
+            state.move_by(consumed_length);
+        } else if state.data[0] == b'l' {
+            let end_index = state.data.iter().position(|&b| b == b'e').unwrap();
+            state.move_by(end_index + 1);
+        } else {
+            let (value, consumed_length) = decode_string(state.data)?;
+            state.move_by(consumed_length);
+        }
+    }
+    state.move_by(1);
+    Ok(())
 }
 
 #[cfg(test)]
@@ -78,6 +115,15 @@ mod tests {
     #[test]
     fn skips_dict_elements() {
         let encoded = "d4:spamd3:fooi1234ee3:cow3:mooe".as_bytes();
+        let (decoded_dict, consumed_length) = decode_dict(encoded).unwrap();
+
+        assert_eq!(1, decoded_dict.len());
+        assert_eq!(consumed_length, encoded.len());
+    }
+
+    #[test]
+    fn skips_list_elements() {
+        let encoded = "d4:spaml4:spam4:eggse3:cow3:mooe".as_bytes();
         let (decoded_dict, consumed_length) = decode_dict(encoded).unwrap();
 
         assert_eq!(1, decoded_dict.len());
