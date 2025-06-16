c941bf8beea0a802bd2403624c7c6f2df6e0fcca
Sergey Moshnikov <s.moshnikov@gmail.com>
2025-06-16 14:50:45 +0200

    Make basic HTTP request by announce URL

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,14 @@
-use bt_client::read_torrent_file;
+use std::error::Error;
 
-fn main() {
+use bt_client::read_torrent_file;
+use reqwest;
+
+fn main() -> Result<(), Box<dyn Error>> {
     let torrent_file_contents = read_torrent_file();
     let announce_url = torrent_file_contents.get_string("announce").unwrap();
     println!("\nYour announce url is: {}", announce_url);
+
+    let response = reqwest::blocking::get(announce_url)?;
+    println!("Response: {:?}", response.text()?);
+    Ok(())
 }
