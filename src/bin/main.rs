c7ac76d388fb7d73db67e9f039df526c8e65af5a
Sergey Moshnikov <s.moshnikov@gmail.com>
2025-11-01 10:23:30 +0100

    Custom deserialization of Info block

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -10,12 +10,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let torrent = read_torrent_file()?;
     let info = torrent.info;
 
-    let info_hash = info.sha1();
-    let piece_hashes = info.piece_hashes();
-
     let announce_request = AnnounceRequest {
-        tracker_url: torrent.announce.clone(),
-        info_hash,
+        tracker_url: torrent.announce,
+        info_hash: info.sha1,
         peer_id,
     };
     let peer_addrs = announce_request.fetch_peer_addresses()?;
@@ -23,12 +20,12 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     println!("* Probing peers...");
     if let Some(mut channel) = probe_peers_sequential(&peer_addrs, |addr| {
-        request_complete_file(addr, &info_hash, &peer_id, piece_hashes.len())
+        request_complete_file(addr, &info.sha1, &peer_id, info.pieces.len())
     }) {
         println!("* Connected to peer: {:?}", channel.peer_addr());
 
         let (file_content, download_duration) =
-            download_file(&mut channel, piece_hashes, info.piece_length, info.length)?;
+            download_file(&mut channel, info.pieces, info.piece_length, info.length)?;
         println!(
             "* Received entire file, first 128 bytes: {}",
             hex::encode(&file_content[..128])
--- src/torrent.rs
+++ src/torrent.rs
@@ -1,15 +1,14 @@
 use crate::types::Sha1;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
 use serde_bytes::ByteBuf;
 use std::fs;
 
-#[derive(Deserialize, Serialize)]
 pub struct Info {
+    pub sha1: Sha1,
     pub name: String,
-    #[serde(rename = "piece length")]
     pub piece_length: u32,
     pub length: usize,
-    pub pieces: ByteBuf,
+    pub pieces: Vec<Sha1>,
 }
 
 #[derive(Deserialize)]
@@ -24,6 +23,38 @@ pub fn read_torrent_file() -> Result<Torrent, Box<dyn std::error::Error>> {
     Ok(decoded)
 }
 
+impl<'de> Deserialize<'de> for Info {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        #[derive(Deserialize, Serialize)]
+        struct InfoInternal {
+            pub name: String,
+            #[serde(rename = "piece length")]
+            pub piece_length: u32,
+            pub length: usize,
+            pub pieces: ByteBuf,
+        }
+
+        let info_internal = InfoInternal::deserialize(deserializer)?;
+        let sha1 = Sha1::calculate(&serde_bencode::to_bytes(&info_internal).unwrap());
+        let pieces = info_internal
+            .pieces
+            .as_slice()
+            .chunks_exact(20)
+            .map(Sha1::from_bytes)
+            .collect::<Vec<_>>();
+
+        Ok(Info {
+            name: info_internal.name,
+            piece_length: info_internal.piece_length,
+            length: info_internal.length,
+            pieces,
+            sha1,
+        })
+    }
+}
 const TORRENT_FILE: &str = "test-data/debian-12.11.0-amd64-netinst.iso.torrent";
 
 #[cfg(test)]
@@ -45,7 +76,7 @@ mod tests {
         assert_eq!(info.piece_length, 262144);
         assert_eq!(info.length, 702545920);
         assert_eq!(
-            info.piece_hashes().len(),
+            info.pieces.len(),
             (info.length / info.piece_length as usize)
         );
     }
@@ -53,21 +84,6 @@ mod tests {
     #[test]
     fn calculate_info_hash() {
         let torrent: Torrent = read_torrent_file().unwrap();
-        assert_eq!(format!("{}", torrent.info.sha1()), INFO_HASH);
-    }
-}
-
-impl Info {
-    pub fn piece_hashes(&self) -> Vec<Sha1> {
-        self.pieces
-            .as_slice()
-            .chunks_exact(20)
-            .map(Sha1::from_bytes)
-            .collect()
-    }
-
-    pub fn sha1(&self) -> Sha1 {
-        let serialized = serde_bencode::to_bytes(&self).unwrap();
-        Sha1::calculate(&serialized)
+        assert_eq!(format!("{}", torrent.info.sha1), INFO_HASH);
     }
 }
