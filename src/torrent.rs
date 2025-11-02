8c276acf5c9c725e4a2243987a80ed0d3c303edd
Sergey Moshnikov <s.moshnikov@gmail.com>
2025-11-02 11:35:02 +0100

    Using serde(from) to convert InfoInternal into Info

--- src/torrent.rs
+++ src/torrent.rs
@@ -1,8 +1,10 @@
 use crate::types::Sha1;
-use serde::{Deserialize, Deserializer, Serialize};
+use serde::{Deserialize, Serialize};
 use serde_bytes::ByteBuf;
 use std::fs;
 
+#[derive(Deserialize)]
+#[serde(from = "InfoInternal")]
 pub struct Info {
     pub sha1: Sha1,
     pub name: String,
@@ -17,27 +19,17 @@ pub struct Torrent {
     pub info: Info,
 }
 
-pub fn read_torrent_file() -> Result<Torrent, Box<dyn std::error::Error>> {
-    let contents = fs::read(TORRENT_FILE)?;
-    let decoded = serde_bencode::from_bytes(&contents)?;
-    Ok(decoded)
+#[derive(Deserialize, Serialize)]
+struct InfoInternal {
+    pub name: String,
+    #[serde(rename = "piece length")]
+    pub piece_length: u32,
+    pub length: usize,
+    pub pieces: ByteBuf,
 }
 
-impl<'de> Deserialize<'de> for Info {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        #[derive(Deserialize, Serialize)]
-        struct InfoInternal {
-            pub name: String,
-            #[serde(rename = "piece length")]
-            pub piece_length: u32,
-            pub length: usize,
-            pub pieces: ByteBuf,
-        }
-
-        let info_internal = InfoInternal::deserialize(deserializer)?;
+impl From<InfoInternal> for Info {
+    fn from(info_internal: InfoInternal) -> Self {
         let sha1 = Sha1::calculate(&serde_bencode::to_bytes(&info_internal).unwrap());
         let pieces = info_internal
             .pieces
@@ -46,15 +38,22 @@ impl<'de> Deserialize<'de> for Info {
             .map(Sha1::from_bytes)
             .collect::<Vec<_>>();
 
-        Ok(Info {
+        Self {
             name: info_internal.name,
             piece_length: info_internal.piece_length,
             length: info_internal.length,
             pieces,
             sha1,
-        })
+        }
     }
 }
+
+pub fn read_torrent_file() -> Result<Torrent, Box<dyn std::error::Error>> {
+    let contents = fs::read(TORRENT_FILE)?;
+    let decoded = serde_bencode::from_bytes(&contents)?;
+    Ok(decoded)
+}
+
 const TORRENT_FILE: &str = "test-data/debian-12.11.0-amd64-netinst.iso.torrent";
 
 #[cfg(test)]
