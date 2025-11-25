f6fa5bbaa246a1c428536b3ad4fb5ec9d8091db0
Sergey Moshnikov <s.moshnikov@gmail.com>
2025-11-25 10:47:33 +0100

    Added tracing, use tracing in main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,8 +4,11 @@ use bt_client::{
     download_file, probe_peers::probe_peers_sequential, request_complete_file,
     torrent::read_torrent_file, tracker::AnnounceRequest, types::PeerId,
 };
+use tracing::{error, info};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt::init();
+
     let peer_id = PeerId::default();
     let torrent = read_torrent_file()?;
     let info = torrent.info;
@@ -16,26 +19,28 @@ fn main() -> Result<(), Box<dyn Error>> {
         peer_id,
     };
     let peer_addrs = announce_request.fetch_peer_addresses()?;
-    println!("* Total {} peers", peer_addrs.len());
+    info!(peer_count = peer_addrs.len(), "Received peer addresses");
 
-    println!("* Probing peers...");
+    info!("Probing peers");
     if let Some(mut channel) = probe_peers_sequential(&peer_addrs, |addr| {
         request_complete_file(addr, &info.sha1, &peer_id, info.pieces.len())
     }) {
-        println!("* Connected to peer: {:?}", channel.peer_addr());
+        info!(
+            peer_address = channel.peer_addr().to_string(),
+            remote_id = channel.remote_id().to_string(),
+            "Connected to peer"
+        );
 
         let (file_content, download_duration) =
             download_file(&mut channel, info.pieces, info.piece_length, info.length)?;
-        println!(
-            "* Received entire file, first 128 bytes: {}",
-            hex::encode(&file_content[..128])
-        );
-        println!(
-            "* File size: {}, download duration: {:?}",
-            info.length, download_duration
+        info!(
+            file_bytes = hex::encode(&file_content[..128]),
+            file_size = info.length,
+            download_duration = format!("{:.2?}", download_duration),
+            "Received entire file"
         );
     } else {
-        println!("* No peer responded");
+        error!("No peer responded");
     }
 
     Ok(())
