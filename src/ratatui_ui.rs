88a30fc4c5a650c8df7aa8cdcb88f552cdd1b190
Sergey Moshnikov <s.moshnikov@gmail.com>
2025-12-22 10:53:31 +0100

    Styling the main UI

--- src/ratatui_ui.rs
+++ src/ratatui_ui.rs
@@ -6,19 +6,23 @@ use std::{
 use ratatui::{
     DefaultTerminal, Frame,
     crossterm::event::{self, Event},
+    style::{Color, Stylize},
+    text::{Line, ToLine},
+    widgets::{Block, Padding, Paragraph},
 };
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 pub enum AppEvent {
     Exit,
+    Resize,
     Probing(String),
     Downloading(usize, usize),
     Completed,
 }
 
 pub struct AppUi {
-    app_state: AppState,
+    app_state: DownloadState,
     event_sender: Sender<AppEvent>,
     event_receiver: Receiver<AppEvent>,
 }
@@ -31,16 +35,11 @@ enum DownloadState {
     Downloading(usize, usize),
 }
 
-#[derive(Default)]
-struct AppState {
-    download_state: DownloadState,
-}
-
 impl AppUi {
     pub fn new() -> Self {
         let (event_sender, event_receiver) = mpsc::channel::<AppEvent>();
         Self {
-            app_state: AppState::default(),
+            app_state: DownloadState::default(),
             event_sender,
             event_receiver,
         }
@@ -70,39 +69,52 @@ impl AppUi {
     fn process_app_event(&mut self) -> Result<bool> {
         match self.event_receiver.recv()? {
             AppEvent::Probing(ip_address) => {
-                self.app_state.download_state = DownloadState::Probing(ip_address);
+                self.app_state = DownloadState::Probing(ip_address);
                 Ok(true)
             }
             AppEvent::Downloading(current, total) => {
-                self.app_state.download_state = DownloadState::Downloading(current, total);
+                self.app_state = DownloadState::Downloading(current, total);
                 Ok(true)
             }
+            AppEvent::Resize => Ok(true),
             AppEvent::Completed => Ok(false),
             AppEvent::Exit => Ok(false),
         }
     }
 
     fn render_ui(&mut self, f: &mut Frame) {
-        let status_text = match &self.app_state.download_state {
-            DownloadState::Idle => "Idle".to_string(),
-            DownloadState::Probing(ip_address) => format!("Probing: {}", ip_address),
+        let status_line = match &self.app_state {
+            DownloadState::Idle => Line::from("Idle").fg(Color::Green),
+            DownloadState::Probing(ip_address) => {
+                Line::from(format!("Probing: {}", ip_address)).fg(Color::Red)
+            }
             DownloadState::Downloading(downloaded, total) => {
-                format!("Downloading: {} / {}", downloaded, total)
+                Line::from(format!("Downloading: {} / {}", downloaded, total)).fg(Color::Yellow)
             }
         };
 
-        let block = ratatui::widgets::Block::default().title(status_text);
-        f.render_widget(block, f.area());
+        let status = Paragraph::new(status_line).block(
+            Block::bordered()
+                .title(" BitTorrent Client ".to_line().centered())
+                .title_bottom(" Press <ESC> to exit ".to_line().centered())
+                .padding(Padding::horizontal(1)),
+        );
+
+        f.render_widget(status, f.area());
     }
 
     fn listen_for_keyboard_events(sender: Sender<AppEvent>) {
         thread::spawn(move || {
             loop {
-                if let Event::Key(key) = event::read().unwrap() {
-                    match key.code {
+                match event::read().unwrap() {
+                    Event::Key(key) => match key.code {
                         event::KeyCode::Esc => sender.send(AppEvent::Exit).unwrap(),
                         _ => {}
+                    },
+                    Event::Resize(_, _) => {
+                        sender.send(AppEvent::Resize).unwrap();
                     }
+                    _ => (),
                 }
             }
         });
