14455bf8609de77bfd00e65b575e264e0376e32b
Sergey Moshnikov <s.moshnikov@gmail.com>
2025-12-27 11:02:08 +0100

    Some cleanup

--- src/bin/main-tui.rs
+++ src/bin/main-tui.rs
@@ -12,18 +12,16 @@ pub fn main() -> Result<()> {
 }
 
 fn download_file(tx: Sender<AppEvent>) {
-    thread::spawn(move || {
-        let ip_addresses = vec!["127.0.0.1:6881", "127.0.0.2:6882", "127.0.0.3:6883"];
-        for ip_address in ip_addresses {
-            tx.send(AppEvent::Probing(ip_address.to_string())).unwrap();
-            thread::sleep(Duration::from_secs(3));
-        }
+    let ip_addresses = vec!["127.0.0.1:6881", "127.0.0.2:6882", "127.0.0.3:6883"];
+    for ip_address in ip_addresses {
+        tx.send(AppEvent::Probing(ip_address.to_string())).unwrap();
+        thread::sleep(Duration::from_secs(3));
+    }
 
-        for i in 0..100 {
-            tx.send(AppEvent::Downloading(i, 100)).unwrap();
-            thread::sleep(Duration::from_millis(100));
-        }
+    for i in 0..100 {
+        tx.send(AppEvent::Downloading(i, 100)).unwrap();
+        thread::sleep(Duration::from_millis(100));
+    }
 
-        tx.send(AppEvent::Completed).unwrap();
-    });
+    tx.send(AppEvent::Completed).unwrap();
 }
