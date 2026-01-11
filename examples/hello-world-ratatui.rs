a5b44957bfa9566e61403fb3fd9b0dd5d92fa056
Sergey Moshnikov <s.moshnikov@gmail.com>
2026-01-11 13:35:05 +0100

    Commenting the code

--- examples/hello-world-ratatui.rs
+++ examples/hello-world-ratatui.rs
@@ -6,9 +6,11 @@ use ratatui::{
 };
 
 pub fn main() {
+    // 1: Initialize the terminal
     let mut terminal = ratatui::init();
-
+    // 2: Enter the render loop
     loop {
+        // 2.1: Render the UI
         terminal
             .draw(|frame| {
                 let text = Line::from("Hello, world!").bold().italic();
@@ -16,7 +18,7 @@ pub fn main() {
                 frame.render_widget(widget, frame.area());
             })
             .expect("failed to draw frame");
-
+        // 2.2: Wait for user input
         match event::read().expect("failed to read event") {
             Event::Key(key) => {
                 if key.code == KeyCode::Esc {
@@ -26,6 +28,6 @@ pub fn main() {
             _ => (),
         }
     }
-
+    // 3: Restore the terminal
     ratatui::restore();
 }
