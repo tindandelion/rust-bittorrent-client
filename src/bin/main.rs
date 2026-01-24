ae044e13a7f7c1ceadbc9b7efddac9a13516ecdb
Sergey Moshnikov <s.moshnikov@gmail.com>
2026-01-24 14:09:44 +0100

    Set up logging to the file; cleanup

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,21 +1,27 @@
+use std::fs::File;
+
 use bt_client::{Torrent, ratatui_ui::App, result::Result};
 use tracing::Level;
 
 pub fn main() -> Result<()> {
-    let torrent = Torrent::read_default_file()?;
+    setup_tracing()?;
 
     let mut ui = App::new();
-    ui.start_background_task(|tx| torrent.download_ui(tx));
+    ui.start_background_task(|tx| Torrent::read_default_file()?.download(tx));
     ui.run_ui_loop()?;
 
     println!("Download completed successfully");
     Ok(())
 }
 
-fn _main() -> Result<()> {
+fn setup_tracing() -> Result<()> {
+    let crate_name = env!("CARGO_PKG_NAME");
+    let log_filename = format!("{}.log", crate_name);
+
     tracing_subscriber::fmt()
         .with_max_level(Level::DEBUG)
+        .with_writer(File::create(&log_filename)?)
         .init();
 
-    Torrent::read_default_file()?.download()
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ use std::{net::SocketAddr, sync::mpsc::Sender};
 pub use torrent::Torrent;
 
 impl Torrent {
-    pub fn download_ui(self, event_sender: &Sender<AppEvent>) -> Result<()> {
+    pub fn download(self, event_sender: &Sender<AppEvent>) -> Result<()> {
         let info = self.info;
 
         let peer_id = PeerId::default();
@@ -67,51 +67,6 @@ impl Torrent {
         }
 
         event_sender.send(AppEvent::Completed)?;
-        Ok(())
-    }
-
-    pub fn download(self) -> Result<()> {
-        let info = self.info;
-
-        let peer_id = PeerId::default();
-        let announce_request = AnnounceRequest {
-            tracker_url: self.announce,
-            info_hash: info.sha1,
-            peer_id,
-        };
-        let peer_addrs = announce_request.fetch_peer_addresses()?;
-        info!(peer_count = peer_addrs.len(), "Received peer addresses");
-
-        info!("Probing peers");
-        if let Some(mut channel) = probe_peers_sequential(&peer_addrs, |addr| {
-            request_complete_file(addr, &peer_id, &info)
-        }) {
-            info!(
-                file_size = info.length,
-                piece_count = info.pieces.len(),
-                peer_address = %channel.peer_addr(),
-                remote_id = %channel.remote_id(),
-                "Downloading file"
-            );
-            let (file_content, download_duration) = util::elapsed(|| {
-                downloader::FileDownloader::new(
-                    &mut channel,
-                    info.pieces,
-                    info.piece_length,
-                    info.length,
-                )
-                .download()
-            })?;
-            info!(
-                file_bytes = hex::encode(&file_content[..128]),
-                file_size = info.length,
-                download_duration = format!("{:.2?}", download_duration),
-                "Received entire file"
-            );
-        } else {
-            error!("No peer responded");
-        }
-
         Ok(())
     }
 }
