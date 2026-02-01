8b4ad4562b5450d34ef461a875ecc8ea3f1477d1
Sergey Moshnikov <s.moshnikov@gmail.com>
2026-02-01 09:16:10 +0100

    Integration test compares files

--- tests/download_file.rs
+++ tests/download_file.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::net::SocketAddr;
 use std::sync::mpsc;
 
@@ -5,14 +6,24 @@ use bt_client::Torrent;
 use bt_client::result::Result;
 use bt_client::types::PeerId;
 
+const DATA_FILE_PATH: &str = "test-env/war-and-peace/war-and-peace.txt";
+const TORRENT_FILE_PATH: &str = "test-env/war-and-peace/war-and-peace.torrent";
+const LOCAL_IP_ADDRESS: &str = "127.0.0.1:51413";
+
 #[test]
 fn test_download_file() -> Result<()> {
-    let peer_address: SocketAddr = "127.0.0.1:51413".parse()?;
-    let torrent = Torrent::read_file("test-env/war-and-peace/war-and-peace.torrent")?;
-
+    let peer_address = LOCAL_IP_ADDRESS.parse()?;
+    let torrent = Torrent::read_file(TORRENT_FILE_PATH)?;
     let peer_id = PeerId::default();
+
     let (tx, _rx) = mpsc::channel();
-    torrent.download_from(peer_id, vec![peer_address], &tx)?;
+    let downloaded = torrent.download_from(peer_id, vec![peer_address], &tx)?;
+    assert_eq!(read_test_file()?, downloaded.content);
 
     Ok(())
+}
+
+fn read_test_file() -> Result<Vec<u8>> {
+    let content = fs::read(DATA_FILE_PATH)?;
+    Ok(content)
 }
