e066d4275937f1be54dfe384e6564f5b74628758
Sergey Moshnikov <s.moshnikov@gmail.com>
2026-02-19 08:37:06 +0100

    An example to dig into connect errors

---
+++ examples/request-file.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+use std::time::Instant;
+
+use bt_client::Torrent;
+use bt_client::result::Result;
+use bt_client::types::PeerId;
+
+fn main() -> Result<()> {
+    let torrent = Torrent::read_default_file()?;
+    let peer_id = PeerId::default();
+    let mut errors = HashMap::<String, usize>::new();
+
+    let addrs = torrent.fetch_peer_addresses(peer_id)?;
+    for addr in addrs {
+        print!("{addr}\t\t\t");
+        let start = Instant::now();
+        let result = torrent.request_file(addr, peer_id);
+        let duration = start.elapsed().as_millis();
+        match result {
+            Ok(_) => println!("OK ({duration}ms)"),
+            Err(e) => {
+                let err_str = e.to_string();
+                println!("Err({err_str}) ({duration}ms)");
+                errors
+                    .entry(err_str)
+                    .and_modify(|count| *count += 1)
+                    .or_insert(1);
+            }
+        }
+    }
+
+    println!("\n\n --- Errors:");
+    for (err, count) in errors {
+        println!("{err}: {count}");
+    }
+
+    Ok(())
+}
